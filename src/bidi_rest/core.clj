4b7d23bcead3c07cf43253522350e744c718a09e
Daniel Braun <danielbraun@me.com>
2017-04-03 13:05:29 +0300

    Indentation

--- src/bidi_rest/core.clj
+++ src/bidi_rest/core.clj
@@ -4,14 +4,14 @@
                        :or {param :id}}]
   (let [action #(keyword (name id) (name %))]
     [(name (or path id)) {"" {:get (action :index)
-                    :post (action :create)}
-                "/new" {:get (action :new)}
-                ["/" param] [["" {:get (action :show)
-                                  :put (action :update)
-                                  :patch (action :update)
-                                  :delete (action :destroy)}]
-                             ["/edit" {:get (action :edit)}]
-                             ["/" children]]}]))
+                              :post (action :create)}
+                          "/new" {:get (action :new)}
+                          ["/" param] [["" {:get (action :show)
+                                            :put (action :update)
+                                            :patch (action :update)
+                                            :delete (action :destroy)}]
+                                       ["/edit" {:get (action :edit)}]
+                                       ["/" children]]}]))
 
 (defn resource [id & {:keys [path]}]
   (let [action #(keyword (name id) (name %))]
