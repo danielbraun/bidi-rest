39f78611896b6daf9034a023ff1a6fd186faca95
Daniel Braun <danielbraun@me.com>
2017-04-10 15:04:07 +0300

    Refactoring, Added tests

--- src/bidi_rest/core.clj
+++ src/bidi_rest/core.clj
@@ -1,24 +1,26 @@
 (ns bidi-rest.core)
 
-(defn resources [id & {:keys [children param path]
-                       :or {param :id}}]
-  (let [action #(keyword (name id) (name %))]
-    [(name (or path id)) {"" {:get (action :index)
-                              :post (action :create)}
-                          "/new" {:get (action :new)}
-                          ["/" param] [["" {:get (action :show)
-                                            :put (action :update)
-                                            :patch (action :update)
-                                            :delete (action :destroy)}]
-                                       ["/edit" {:get (action :edit)}]
-                                       ["/" children]]}]))
+(defn- make-resource [singular? id & {:keys [children param path]
+                                      :or {param :id}}]
+  (let [action #(keyword (name id) (name %))
+        create [:post (action :create)]
+        destroy [:delete (action :destroy)]
+        edit ["/edit" {:get (action :edit)}]
+        index [:get (action :index)]
+        new ["/new" {:get (action :new)}]
+        show [:get (action :show)]
+        update [[:patch (action :update)]
+                [:put (action :update)]]]
+    [(name (or path id))
+     (if singular?
+       [["" (into [destroy show create] update)]
+        edit
+        new]
+       [["" [index create]]
+        new
+        [["/" param] [["" (into [destroy show] update)]
+                      edit
+                      ["/" children]]]])]))
 
-(defn resource [id & {:keys [path]}]
-  (let [action #(keyword (name id) (name %))]
-    [(name (or path id)) {"" {:delete (action :destroy)
-                              :get (action :show)
-                              :patch (action :update)
-                              :post (action :create)
-                              :put (action :update)}
-                          "/edit" {:get (action :edit)}
-                          "/new" {:get (action :new)}}]))
+(def resources (partial make-resource false))
+(def resource (partial make-resource true))
--- test/bidi_rest/core_test.clj
+++ test/bidi_rest/core_test.clj
@@ -1,7 +1,29 @@
 (ns bidi-rest.core-test
   (:require [clojure.test :refer :all]
-            [bidi-rest.core :refer :all]))
+            [bidi-rest.core :refer :all]
+            [bidi.bidi :as bidi]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def routes
+  ["/" [(resources :stars)
+        (resource :universe)]])
+
+(defn- match-request [[method uri]]
+  (:handler (bidi/match-route routes uri :request-method method)))
+
+(deftest routes-test
+  (are [handler request] (= handler (match-request request))
+       :stars/create [:post "/stars"]
+       :stars/destroy [:delete "/stars/1"]
+       :stars/edit [:get "/stars/1/edit"]
+       :stars/index [:get "/stars"]
+       :stars/new [:get "/stars/new"]
+       :stars/show [:get "/stars/1"]
+       :stars/update [:patch "/stars/1"]
+       :stars/update [:put "/stars/1"]
+       :universe/create [:post "/universe"]
+       :universe/destroy [:delete "/universe"]
+       :universe/edit [:get "/universe/edit"]
+       :universe/new [:get "/universe/new"]
+       :universe/show [:get "/universe"]
+       :universe/update [:patch "/universe"]
+       :universe/update [:put "/universe"]))
