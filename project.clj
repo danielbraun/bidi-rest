c2415f56f2aee83fbf6480f6f4a4f487149edd73
Daniel Braun <danielbraun@me.com>
2017-12-19 14:30:00 +0200

    Added description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bidi-rest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Rails-like resourceful routing for Bidi"
+  :url "https://github.com/danielbraun/bidi-rest"
   :cljsbuild {:builds []}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
